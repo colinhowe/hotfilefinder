946eed667495c922b834b5c96c3e9100341f682e
Colin Howe <colinthehowe@gmail.com>
2014-08-20 22:50:09 +0100

    Account for created and deleted files

--- crappyfilefinder/__init__.py
+++ crappyfilefinder/__init__.py
@@ -5,7 +5,9 @@ import subprocess
 DESCRIPTION = """Process a git repo's history and list out all the files in
 order of their hotness.
 
-Hotness is the number of lines that have been modified."""
+Hotness is the number of lines that have been modified. This count excludes
+changes due to creation/deletion of entire files. E.g. if a file is created
+with 10 lines then 5 more are added, the hotness will be 5."""
 
 def calculate_hotness(path, since=None, until=None):
     """
@@ -19,6 +21,8 @@ def calculate_hotness(path, since=None, until=None):
     """
     log = _get_git_log(path, since=since, until=until)
     commits = _group_into_commits(log)
+    # Process commits in reverse order so that deleted files are ignored.
+    commits = reversed(list(commits))
     changes = [_parse_commit_change_size(commit) for commit in commits]
     totals = _combine_changes(changes)
     return totals
@@ -27,7 +31,7 @@ def _get_git_log(path, since, until):
     """
     Gets the git log at the given path.
     """
-    args = ['git', 'log', '--numstat', '--format=format:']
+    args = ['git', 'log', '--summary', '--numstat', '--format=format:']
     if since:
         args.append('--since=%s' % since)
     if until:
@@ -54,17 +58,47 @@ def _group_into_commits(log):
         yield commit
 
 def _parse_commit_change_size(commit):
+    """
+    Parses git's change stat format:
+        5 10 blah.py
+    Indicates 5 new lines, 10 removed lines in blah.py
+
+    This will exclude any changes due to creation.
+
+    If a file has been deleted it will be returned with a value of -1.
+    """
     result = {}
     for line in commit:
-        incr, decr, path = line.split('\t')
-        result[path] = int(incr) + int(decr)
+        if line.startswith(' delete'):
+            path = line.split(' ', 4)[-1]
+            result[path] = -1
+        elif line.startswith(' create'):
+            path = line.split(' ', 4)[-1]
+            if path in result:
+                del result[path]
+        elif line.startswith(' mode'):
+            continue
+        else:
+            try:
+                incr, decr, path = line.split('\t')
+            except ValueError:
+                print '\n'.join(commit)
+                raise
+            # binary files are specified as - - instead of number differences
+            if incr == '-' or decr == '-':
+                continue
+            result[path] = int(incr) + int(decr)
     return result
 
 def _combine_changes(commits):
     result = collections.defaultdict(int)
     for commit in commits:
         for path, value in commit.iteritems():
-            result[path] += value
+            if value == -1:
+                if path in result:
+                    del result[path]
+            else:
+                result[path] += value
     return result
 
 def _process_args():
@@ -75,8 +109,9 @@ def _process_args():
         help='date to limit commits from')
     parser.add_argument('--until',
         help='date to limit commits until')
-    parser.add_argument('-n', type=int, default=50,
-        help='number of results to output. Specifying 0 will output all')
+    parser.add_argument('-n', type=int, default=20,
+        help='number of results to output. Specifying 0 will output all.'
+        + ' Default 20')
     return parser.parse_args()
 
 if __name__ == '__main__':
